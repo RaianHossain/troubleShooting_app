794b1ab9ac590fccfc6ab913b7d7fbfbe9c4b9c4
Jannatul Ferdows Modina <jannat.bubtcse@gmail.com>
2022-08-30 23:53:49 +0600

    dashboard controller fixes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Mail;
+use App\Models\Notification;
+use Carbon\Carbon;
 use App\Mail\sendingEmail;
 use Illuminate\Http\Request;
 
@@ -9,7 +11,20 @@ class DashboardController extends Controller
 {
     public function dashboard()
     {
-        return view('dashboard.dashboard');
+        $allNotifications = Notification::orderBy('id', 'DESC')->simplePaginate(20);      
+        $notifications = array();
+        foreach($allNotifications as $allNotification)
+        {
+            $subscribers = unserialize($allNotification->subscriber);
+            if(in_array(auth()->user()->id, array_keys($subscribers))){
+                array_push($notifications, $allNotification);
+            }
+        }
+
+        $notifications = (object) $notifications;
+        // dd($notifications);
+        return view('dashboard.dashboard', compact('notifications'));
+        //return view('dashboard.dashboard');
     }
     
     public function sendEmail()
